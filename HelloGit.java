0e64d5a8652df41aa87344bdcf46e96a4ba17395
raywt <raywt@users.noreply.github.com>
2015-09-12 14:01:53 +0100

    added goodnight

--- HelloGit.java
+++ HelloGit.java
@@ -1,5 +1,6 @@
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello, Git! Hello, GitHub!");
+		System.out.println("Goodnight GitHub, Goodnight Brave Git!");
 	}
 }
