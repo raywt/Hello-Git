4387077e648c5a305a1787ee742b3c08240bd443
ray <raywt@users.noreply.github.com>
2015-10-17 12:42:50 +0100

    new feature

--- HelloGit.java
+++ HelloGit.java
@@ -1,6 +1,5 @@
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello, Git! Hello, GitHub!");
-		System.out.println("Goodnight GitHub, Goodnight Brave Git!");
 	}
 }
